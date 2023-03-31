7eaacd7fd3b29358814d6eeda3fd9d3bdb81b5f1
Mark Fonte <mark@fonte.com>
2023-03-31 23:27:25 +0000

    Send 200 back on redeploy

--- redeploy.js
+++ redeploy.js
@@ -4,8 +4,8 @@ const port = 6969
 const { exec } = require('child_process');
 
 app.post('/webhook', (req, res) => {
-		exec('./redeploy_script.sh start', (err, stdout, stderr) => {console.log(stdout);});
-	}
-)
+	exec('./redeploy_script.sh start', (err, stdout, stderr) => {console.log(stdout);});
+	res.sendStatus(200);
+})
 
 app.listen(port, () => console.log(`Redeploy script listening on port ${port}!`))
